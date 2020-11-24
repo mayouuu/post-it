c335a5e2175e48ec5d512435de5440e5c04eeff0
maelle <maelle.defay@epitech.eu>
2020-11-24 16:26:47 +0100

    (ADD]

---
+++ post-it/src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="card">
+
+
+        </div>
+    </div> 
+</template>
+
+<script>
+    export default {
+        name: 'List'
+    }
+</script>
+
+<style>
+.card {
+    height: 50%;
+    width: 20%;
+    background-color: aqua;
+}
+</style>
